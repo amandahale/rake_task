3a95611140bfe696399e464455d04207301ab9d0
amanda <forshagonoff@gmail.com>
2024-06-27 14:44:50 -0500

    creates run action

--- app/controllers/rake_tasks_controller.rb
+++ app/controllers/rake_tasks_controller.rb
@@ -1,2 +1,9 @@
 class RakeTasksController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: :run
+  def run
+    # task_name = params[:task_name]
+    # task_params = params[:task_params] || {}
+    
+    render json: { message: "yooo" }
+  end
 end
